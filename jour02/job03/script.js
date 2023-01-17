5117d6a095c97a333fd454490704eaf491eb6462
felix-hauger <felix.hauger@laplateforme.io>
2023-01-17 11:25:13 +0100

    jour02, job03 - reduced addOne function size

--- jour02/job03/script.js
+++ jour02/job03/script.js
@@ -1,10 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
     const button = document.querySelector('#button'),
         addOne = function () {
-            const countParagraph = document.querySelector('#count');
-            let count = parseInt(countParagraph.textContent);
-            count++;
-            countParagraph.textContent = count;
+            const count = document.querySelector('#count');
+            count.textContent = parseInt(count.textContent) + 1;
         }
 
     button.addEventListener('click', addOne);
