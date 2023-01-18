9118970eaf53005be19b855a8aabaf3d4e8ab728
felix-hauger <felix.hauger@laplateforme.io>
2023-01-18 15:30:15 +0100

    jour02, job06 - change style when user type konami code

---
+++ jour02/job06/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="script.js"></script>
+    <title>Job 06</title>
+</head>
+    
+<body>
+    <h1>Mon beau titre</h1>
+    <p id="keylogger"></p>
+</body>
+
+</html>
---
+++ jour02/job06/script.js
@@ -0,0 +1,40 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const konami = function(event) {
+        // to compare with user input using session variable count as an index
+        const konamiCodeArray = [
+            'ArrowUp', 
+            'ArrowUp',
+            'ArrowDown',
+            'ArrowDown',
+            'ArrowLeft',
+            'ArrowRight',
+            'ArrowLeft',
+            'ArrowRight',
+            'b',
+            'a'
+        ];
+
+        // set the variable in the session storage
+        if (sessionStorage.getItem('count') == null) {
+            sessionStorage.setItem('count', 0);
+        } else {
+            // if the input correspond to the count of the array
+            if (event.key == konamiCodeArray[sessionStorage.getItem('count')]) {
+
+                // increment session variable
+                sessionStorage.setItem('count' , parseInt(sessionStorage.getItem('count')) + 1)
+            } else {
+                // else reset count variable
+                sessionStorage.setItem('count', 0);
+            }
+        }
+
+        // if all inputs match the count will be equal to array size (10)
+        if (sessionStorage.getItem('count') == 10) {
+            console.log('KOMONEY');
+            document.querySelector('*').style = 'background-color: #336699; color: white;';
+        }
+    }
+
+    document.addEventListener('keydown', konami);
+});
