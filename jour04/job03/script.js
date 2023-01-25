5c604eb6105d5e9b5adf455832dca91e6d3dacd2
felix-hauger <felix.hauger@laplateforme.io>
2023-01-25 17:15:00 +0100

    jour04, job03 - filter id, name & type separately

--- jour04/job03/script.js
+++ jour04/job03/script.js
@@ -4,18 +4,60 @@ document.addEventListener('DOMContentLoaded', () => {
         let id = document.querySelector('#id').value,
             name = document.querySelector('#name').value,
             type = document.querySelector('#type').value;
-
-        console.log(id, name, type);
+        let filters = {
+            "id": document.querySelector('#id').value,
+            "name": document.querySelector('#name').value,
+            "type": document.querySelector('#type').value
+        };
+        // console.log(id, name, type);
         fetch('pokemon.json')
             .then((response) => {
-                console.log(response);
+                // console.log(response);
+                // console.log(id);
                 return response.json();
             })
             .then((json) => {
-                console.log(json);
-                // for (let i = 0; i < json.length; i++) {
-                //     console.log(json[i].name.french);
+                // console.log(json);
+                // if (id >= 1) {
+                //     console.log(json[filters.id-1].name.french);
+                //     console.log(filters.id);
                 // }
+                // console.log(filters);
+                // console.log(filters.id);
+                // console.log(json[filters.id - 1].name.french);
+                for (const filter in filters) {
+                    if (Object.hasOwnProperty.call(filters, filter)) {
+                        const element = filters[filter];
+                        console.log(typeof element);
+                    }
+                }
+
+                for (let i = 0; i < json.length; i++) {
+                    // console.log(Object.entries(json[i]));
+                    // console.log(json[i]);
+                    // console.log(filters.id - i)
+
+                    if (filters.id != '' && i == filters.id - 1) {
+                        console.log(json[i].name.french);
+                        let entry = document.createElement('tr'),
+                            id = document.createElement('td'),
+                            name;
+
+                    }
+                    if (filters.type != '' && Object.values(json[i].type).includes(filters.type)) {
+                        // console.log()
+                        console.log(json[i].name.french);
+                        let entry = document.createElement('tr'),
+                            id = document.createElement('td'),
+                            name;
+
+                    }
+                    if (filters.name != '' && Object.values(json[i].name).includes(filters.name)) {
+                        console.log(json[i].name.french);
+                    }
+                        
+                    
+                }
             });
     }
 
