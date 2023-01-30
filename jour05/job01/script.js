9468ddf38048d34f5acfa46a5640957c1b55f49f
felix-hauger <felix.hauger@laplateforme.io>
2023-01-30 15:00:06 +0100

    jour05, job01 - added basic code structure in script.js

---
+++ jour05/job01/script.js
@@ -0,0 +1,14 @@
+const connectButton = document.querySelector('#connect'),
+    registerButton = document.querySelector('#register');
+
+const connect = function() {
+
+}
+
+const register = function() {
+
+}
+
+
+connectButton.addEventListener('click', connect);
+registerButton.addEventListener('click', register);
